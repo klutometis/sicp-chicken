642045c96421b5fc33e0da8935939a41b8892e1f
Peter Danenberg <danenberg@google.com>
2014-06-22 13:35:32 -0700

    Add some actual code.

--- sicp.scm
+++ sicp.scm
@@ -1,7 +1,30 @@
 (module sicp
+  (average
+   dec
+   epsilon
+   inc
+   good-enough?
+   square
+   xor)
+  (import chicken scheme)
 
-()
+  (define inc add1)
 
-(import chicken scheme)
+  (define dec sub1)
 
-)
+  (define epsilon (make-parameter 0.00001))
+  
+  (define (square x) (* x x))
+  
+  (define (good-enough? old-guess guess)
+    (< (abs (- old-guess guess)) (epsilon)))
+  
+  (define (average x y) (/ (+ x y) 2))
+
+  (define-syntax xor
+    (ir-macro-transformer
+     (lambda (expression inject compare)
+       (let ((x (cadr expression))
+             (y (caddr expression)))
+         `(and (or ,x ,y)
+               (not (and ,x ,y))))))))

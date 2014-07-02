dc8a9cab1e89a17b2076365d8c1651a261ba868f
Peter Danenberg <danenberg@google.com>
2014-07-02 07:38:27 -0700

    Add `prime?'.

--- sicp.scm
+++ sicp.scm
@@ -5,15 +5,17 @@
    default-timeout
    enumerate-interval
    epsilon
+   fast-prime?
    flatmap
    inc
    good-enough?
    nil
+   prime?
    square
    terminates?
    timeout-value?
    xor)
-  (import chicken scheme)
+  (import chicken extras scheme)
   (use srfi-18)
 
   (define inc add1)
@@ -65,4 +67,37 @@
         (cons low (enumerate-interval (+ low 1) high))))
 
   (define (flatmap proc seq)
-    (accumulate append nil (map proc seq))))
+    (accumulate append nil (map proc seq)))
+
+  (define (smallest-divisor n) (find-divisor n 2))
+
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+  (define (divides? a b) (= (remainder b a) 0))
+
+  (define (prime? n)
+    (= n (smallest-divisor n)))
+
+  (define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder
+            (square (expmod base (/ exp 2) m))
+            m))
+          (else
+           (remainder
+            (* base (expmod base (- exp 1) m))
+            m))))
+
+  (define (fermat-test n)
+    (define (try-it a)
+      (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+  (define (fast-prime? n times)
+    (cond ((= times 0) #t)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+          (else #f))))
